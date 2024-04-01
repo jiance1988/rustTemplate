519257ec519392579d44a64a74e6fe8ac14cbd15
Jesse <410444720@qq.com>
2024-04-01 19:32:44 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use serde::{Deserialize, Serialize};
+
 fn main() {
     println!("Hello, world!");
+    println!("sql error: {:?}", SqlError {
+        #[cfg(feature = "modbus")]
+        a: 1,
+        #[cfg(feature = "modbus")]
+        b: 2,
+        #[cfg(feature = "modbus")]
+        c: 3,
+    });
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SqlError {
+    #[cfg(feature = "modbus")]
+    a: i32,
+    #[cfg(feature = "modbus")]
+    b: i32,
+    #[cfg(feature = "modbus")]
+    c: i32,
 }
